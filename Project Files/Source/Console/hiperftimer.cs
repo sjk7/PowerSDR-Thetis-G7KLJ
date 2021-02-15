00f649eebb364e22f9a1d15ee15d1521b654c892
Nobba <radiowebmasters@gmail.com>
2021-02-15 19:01:13 +0000

    trying another way to do meters

--- Project Files/Source/Console/hiperftimer.cs
+++ Project Files/Source/Console/hiperftimer.cs
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 // You may contact us via email at: sales@flex-radio.com.
-// Paper mail may be sent to: 
+// Paper mail may be sent to:
 //    FlexRadio Systems
 //    8900 Marybank Dr.
 //    Austin, TX 78750
@@ -34,95 +34,73 @@ using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace Thetis
-{
-    public class HiPerfTimer
-    {
-        [DllImport("Kernel32.dll")]
-        private static extern bool QueryPerformanceCounter(
-            out long lpPerformanceCount);
+namespace Thetis {
+public class HiPerfTimer {
+    [DllImport("Kernel32.dll")]
+    private static extern bool QueryPerformanceCounter(
+        out long lpPerformanceCount);
 
-        [DllImport("Kernel32.dll")]
-        private static extern bool QueryPerformanceFrequency(
-            out long lpFrequency);
+    [DllImport("Kernel32.dll")]
+    private static extern bool QueryPerformanceFrequency(out long lpFrequency);
 
-        private long startTime, stopTime, elapsedTime;
-        private long freq;
+    private long startTime, stopTime, elapsedTime;
+    private long freq;
 
-        // Constructor
-        public HiPerfTimer()
-        {
-            startTime = 0;
-            stopTime = 0;
-            elapsedTime = 0;
+    // Constructor
+    public HiPerfTimer() {
+        startTime = 0;
+        stopTime = 0;
+        elapsedTime = 0;
 
-            if (QueryPerformanceFrequency(out freq) == false)
-            {
-                // high-performance counter not supported
-                throw new Exception();
-            }
-        }
-
-        // Start the timer
-        public void Start()
-        {
-            // let the waiting threads do their work - start on fresh timeslice
-            Thread.Sleep(0);
-
-            QueryPerformanceCounter(out startTime);
-        }
-
-        // Stop the timer
-        public void Stop()
-        {
-            QueryPerformanceCounter(out stopTime);
-        }
-
-        // Returns the duration of the timer (in seconds)
-        public double Duration
-        {
-            get
-            {
-                return (double)(stopTime - startTime) / (double)freq;
-            }
-        }
-
-        public double DurationMsec
-        {
-            get
-            {
-                return (1000.0) * (double)((stopTime - startTime)) / (double)freq;
-            }
-        }
-
-        public double Elapsed
-        {
-            get
-            {
-                QueryPerformanceCounter(out elapsedTime);
-                return (double)(elapsedTime - startTime) / (double)freq;
-            }
-        }
-
-        public void Reset()
-        {
-            Start();
-        }
-
-        public double ElapsedMsec
-        {
-            get
-            {
-                QueryPerformanceCounter(out elapsedTime);
-                return (1000.0) * (double)((elapsedTime - startTime)) / (double)freq;
-            }
-        }
-
-        public long GetFreq()
-        {
-            long freq = 0;
-            QueryPerformanceFrequency(out freq);
-            return freq;
+        if (QueryPerformanceFrequency(out freq) == false) {
+            // high-performance counter not supported
+            throw new Exception();
         }
     }
+
+    // Start the timer
+    public void Start() {
+        // let the waiting threads do their work - start on fresh timeslice
+        Thread.Sleep(0);
+
+        QueryPerformanceCounter(out startTime);
+    }
+
+    // Stop the timer
+    public void Stop() { QueryPerformanceCounter(out stopTime); }
+
+    // Returns the duration of the timer (in seconds)
+    public double Duration {
+        get { return (double)(stopTime - startTime) / (double)freq; }
+    }
+
+    public double DurationMsec {
+        get {
+            return (1000.0) * (double)((stopTime - startTime)) / (double)freq;
+        }
+    }
+
+    public double Elapsed {
+        get {
+            QueryPerformanceCounter(out elapsedTime);
+            return (double)(elapsedTime - startTime) / (double)freq;
+        }
+    }
+
+    public void Reset() { Start(); }
+
+    public double ElapsedMsec {
+        get {
+            QueryPerformanceCounter(out elapsedTime);
+            return (1000.0) * (double)((elapsedTime - startTime))
+                / (double)freq;
+        }
+    }
+
+    public long GetFreq() {
+        long freq = 0;
+        QueryPerformanceFrequency(out freq);
+        return freq;
+    }
+}
 }
