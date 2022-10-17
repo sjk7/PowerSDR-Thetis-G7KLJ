050161ac618866746c7086ea1abb4fd89e3cdeb3
sjk7 <radiowebmasters@gmail.com>
2022-10-17 02:34:18 +0100

    Adding PVS-Studio comments to allow analysis to wdsp

--- Project Files/Source/wdsp/wcpAGC.h
+++ Project Files/Source/wdsp/wcpAGC.h
@@ -1,3 +1,8 @@
+// This is an independent project of an individual developer. Dear PVS-Studio,
+// please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// http://www.viva64.com
 /*  wcpAGC.h
 
 This file is part of a program that implements a Software-Defined Radio.
@@ -18,7 +23,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-The author can be reached by email at  
+The author can be reached by email at
 
 warren@wpratt.com
 
@@ -27,167 +32,150 @@ warren@wpratt.com
 #ifndef _wcpagc_h
 #define _wcpagc_h
 
-#define MAX_SAMPLE_RATE		(384000.0)
-#define MAX_N_TAU			(8)
-#define MAX_TAU_ATTACK		(0.01)
-#define RB_SIZE				(int)(MAX_SAMPLE_RATE * MAX_N_TAU * MAX_TAU_ATTACK + 1)
+#define MAX_SAMPLE_RATE (384000.0)
+#define MAX_N_TAU (8)
+#define MAX_TAU_ATTACK (0.01)
+#define RB_SIZE (int)(MAX_SAMPLE_RATE * MAX_N_TAU * MAX_TAU_ATTACK + 1)
 
-#define AGCPORT				__declspec(dllexport)
+#define AGCPORT __declspec(dllexport)
 
-typedef struct _wcpagc
-{
-	int run;
-	int mode;
-	int pmode;
-	double* in;
-	double* out;
-	int io_buffsize;
-	double sample_rate;
+typedef struct _wcpagc {
+    int run;
+    int mode;
+    int pmode;
+    double* in;
+    double* out;
+    int io_buffsize;
+    double sample_rate;
 
-	double tau_attack;
-	double tau_decay;
-	int n_tau;
-	double max_gain;
-	double var_gain;
-	double fixed_gain;
-	double min_volts;
-	double max_input;
-	double out_targ;
-	double out_target;
-	double inv_max_input;
-	double slope_constant;
+    double tau_attack;
+    double tau_decay;
+    int n_tau;
+    double max_gain;
+    double var_gain;
+    double fixed_gain;
+    double min_volts;
+    double max_input;
+    double out_targ;
+    double out_target;
+    double inv_max_input;
+    double slope_constant;
 
-	double gain;
-	double inv_out_target;
+    double gain;
+    double inv_out_target;
 
-	int out_index;
-	int in_index;
-	int attack_buffsize;
+    int out_index;
+    int in_index;
+    int attack_buffsize;
 
-	double* ring;
-	double* abs_ring;
-	int ring_buffsize;
-	double ring_max;
+    double* ring;
+    double* abs_ring;
+    int ring_buffsize;
+    double ring_max;
 
-	double attack_mult;
-	double decay_mult;
-	double volts;
-	double save_volts;
-	double out_sample[2];
-	double abs_out_sample;
-	int state;
+    double attack_mult;
+    double decay_mult;
+    double volts;
+    double save_volts;
+    double out_sample[2];
+    double abs_out_sample;
+    int state;
 
-	double tau_fast_backaverage;
-	double fast_backmult;
-	double onemfast_backmult;
-	double fast_backaverage;
-	double tau_fast_decay;
-	double fast_decay_mult;
-	double pop_ratio;
+    double tau_fast_backaverage;
+    double fast_backmult;
+    double onemfast_backmult;
+    double fast_backaverage;
+    double tau_fast_decay;
+    double fast_decay_mult;
+    double pop_ratio;
 
-	int hang_enable;
-	double hang_backaverage;
-	double tau_hang_backmult;
-	double hang_backmult;
-	double onemhang_backmult;
-	int hang_counter;
-	double hangtime;
-	double hang_thresh;
-	double hang_level;
+    int hang_enable;
+    double hang_backaverage;
+    double tau_hang_backmult;
+    double hang_backmult;
+    double onemhang_backmult;
+    int hang_counter;
+    double hangtime;
+    double hang_thresh;
+    double hang_level;
 
-	double tau_hang_decay;
-	double hang_decay_mult;
-	int decay_type;
+    double tau_hang_decay;
+    double hang_decay_mult;
+    int decay_type;
 } wcpagc, *WCPAGC;
 
-extern void loadWcpAGC (WCPAGC a);
+extern void loadWcpAGC(WCPAGC a);
 
-extern void xwcpagc (WCPAGC a);
+extern void xwcpagc(WCPAGC a);
 
-extern WCPAGC create_wcpagc (	int run,
-								int mode,
-								int pmode,
-								double* in,
-								double* out,
-								int io_buffsize,
-								int sample_rate,
-								double tau_attack,
-								double tau_decay,
-								int n_tau,
-								double max_gain,
-								double var_gain,
-								double fixed_gain,
-								double max_input,
-								double out_targ,
-								double tau_fast_backaverage,
-								double tau_fast_decay,
-								double pop_ratio,
-								int hang_enable,
-								double tau_hang_backmult,
-								double hangtime,
-								double hang_thresh,
-								double tau_hang_decay
-								);
+extern WCPAGC create_wcpagc(int run, int mode, int pmode, double* in,
+    double* out, int io_buffsize, int sample_rate, double tau_attack,
+    double tau_decay, int n_tau, double max_gain, double var_gain,
+    double fixed_gain, double max_input, double out_targ,
+    double tau_fast_backaverage, double tau_fast_decay, double pop_ratio,
+    int hang_enable, double tau_hang_backmult, double hangtime,
+    double hang_thresh, double tau_hang_decay);
 
-extern void destroy_wcpagc (WCPAGC a);
+extern void destroy_wcpagc(WCPAGC a);
 
-extern void flush_wcpagc (WCPAGC a);
+extern void flush_wcpagc(WCPAGC a);
 
-extern void setBuffers_wcpagc (WCPAGC a, double* in, double* out);
+extern void setBuffers_wcpagc(WCPAGC a, double* in, double* out);
 
-extern void setSamplerate_wcpagc (WCPAGC a, int rate);
+extern void setSamplerate_wcpagc(WCPAGC a, int rate);
 
-extern void setSize_wcpagc (WCPAGC a, int size);
+extern void setSize_wcpagc(WCPAGC a, int size);
 
 // RXA Properties
 
-extern AGCPORT void SetRXAAGCMode (int channel, int mode);
+extern AGCPORT void SetRXAAGCMode(int channel, int mode);
 
-extern AGCPORT void SetRXAAGCFixed (int channel, double fixed_agc);
+extern AGCPORT void SetRXAAGCFixed(int channel, double fixed_agc);
 
-extern AGCPORT void SetRXAAGCAttack (int channel, int attack);
+extern AGCPORT void SetRXAAGCAttack(int channel, int attack);
 
-extern AGCPORT void SetRXAAGCDecay (int channel, int decay);
+extern AGCPORT void SetRXAAGCDecay(int channel, int decay);
 
-extern AGCPORT void SetRXAAGCHang (int channel, int hang);
+extern AGCPORT void SetRXAAGCHang(int channel, int hang);
 
-extern AGCPORT void GetRXAAGCHangLevel(int channel, double *hangLevel);
+extern AGCPORT void GetRXAAGCHangLevel(int channel, double* hangLevel);
 
 extern AGCPORT void SetRXAAGCHangLevel(int channel, double hangLevel);
 
-extern AGCPORT void GetRXAAGCHangThreshold(int channel, int *hangthreshold);
+extern AGCPORT void GetRXAAGCHangThreshold(int channel, int* hangthreshold);
 
-extern AGCPORT void SetRXAAGCHangThreshold (int channel, int hangthreshold);
+extern AGCPORT void SetRXAAGCHangThreshold(int channel, int hangthreshold);
 
-extern AGCPORT void GetRXAAGCTop(int channel, double *max_agc);
+extern AGCPORT void GetRXAAGCTop(int channel, double* max_agc);
 
-extern AGCPORT void SetRXAAGCTop (int channel, double max_agc);
+extern AGCPORT void SetRXAAGCTop(int channel, double max_agc);
 
-extern AGCPORT void SetRXAAGCSlope (int channel, int slope);
+extern AGCPORT void SetRXAAGCSlope(int channel, int slope);
 
-extern AGCPORT void SetRXAAGCThresh(int channel, double thresh, double size, double rate);
+extern AGCPORT void SetRXAAGCThresh(
+    int channel, double thresh, double size, double rate);
 
-extern AGCPORT void GetRXAAGCThresh(int channel, double *thresh, double size, double rate);
+extern AGCPORT void GetRXAAGCThresh(
+    int channel, double* thresh, double size, double rate);
 
 // TXA Properties
 
-extern AGCPORT void SetTXAALCSt (int channel, int state);
+extern AGCPORT void SetTXAALCSt(int channel, int state);
 
-extern AGCPORT void SetTXAALCAttack (int channel, int attack);
+extern AGCPORT void SetTXAALCAttack(int channel, int attack);
 
-extern AGCPORT void SetTXAALCDecay (int channel, int decay);
+extern AGCPORT void SetTXAALCDecay(int channel, int decay);
 
-extern AGCPORT void SetTXAALCHang (int channel, int hang);
+extern AGCPORT void SetTXAALCHang(int channel, int hang);
 
-extern AGCPORT void SetTXALevelerSt (int channel, int state);
+extern AGCPORT void SetTXALevelerSt(int channel, int state);
 
-extern AGCPORT void SetTXALevelerAttack (int channel, int attack);
+extern AGCPORT void SetTXALevelerAttack(int channel, int attack);
 
-extern AGCPORT void SetTXALevelerDecay (int channel, int decay);
+extern AGCPORT void SetTXALevelerDecay(int channel, int decay);
 
-extern AGCPORT void SetTXALevelerHang (int channel, int hang);
+extern AGCPORT void SetTXALevelerHang(int channel, int hang);
 
-extern AGCPORT void SetTXALevelerTop (int channel, double maxgain);
+extern AGCPORT void SetTXALevelerTop(int channel, double maxgain);
 
 #endif
-
